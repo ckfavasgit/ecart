cb663704c37234e29483036137fa84c6b245fc8f
favas.ck <favas.ck@inapp.com>
2025-06-27 15:40:23 +0530

    product detail url modification

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from .views import ProductListAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path('', ProductListAPIView.as_view(), name='product-list'),
-    path('/<int:pk>', ProductDetailAPIView.as_view(), name='product-detail'),
+    path('<int:pk>', ProductDetailAPIView.as_view(), name='product-detail'),
 ]
